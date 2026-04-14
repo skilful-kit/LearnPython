b80559b1b87c53a5d52a2a412cba1c6191a1e36a
skilful-kit <skilfulkitsigma@gmail.com>
2026-04-14 09:26:17 +0200

    Update main.py

--- homework/2026_02_20/3_our_cache/main.py
+++ homework/2026_02_20/3_our_cache/main.py
@@ -1,5 +1,25 @@
 def cache(func):
-    pass
+    storage = {}
 
+    def wrapper(a, b):
+        key = (a, b)
+
+        if key in storage:
+            return storage[key]
+
+        result = func(a, b)
+        storage[key] = result
+        return result
+
+    return wrapper
+
+
+@cache
 def my_sum(a, b):
+    print("Подсчет")
     return a + b
+
+
+print(my_sum(2, 3))
+print(my_sum(2, 3))
+print(my_sum(5, 1))
