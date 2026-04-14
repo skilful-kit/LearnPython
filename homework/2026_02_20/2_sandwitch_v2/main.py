d6dc30eaa30a1089acf4f19e9ab8808530144920
skilful-kit <skilfulkitsigma@gmail.com>
2026-04-14 09:24:32 +0200

    Update main.py

--- homework/2026_02_20/2_sandwitch_v2/main.py
+++ homework/2026_02_20/2_sandwitch_v2/main.py
@@ -1,20 +1,38 @@
 def bread(func):
-    pass
+    def wrapper():
+        return "Bread\n" + func() + "Bread\n"
+    return wrapper
+
 
 def salat(func):
-    pass
+    def wrapper():
+        return "Salat\n" + func()
+    return wrapper
+
 
 def tomato(func):
-    pass
+    def wrapper():
+        return "Tomato\n" + func()
+    return wrapper
+
 
 def meat(func):
-    pass
+    def wrapper():
+        return "Meat\n" + func()
+    return wrapper
 
+
+@bread
+@salat
+@tomato
+@meat
 def make_sandwich():
     return ''
 
+
 def main():
-    make_sandwich()
+    print(make_sandwich(), end='')
+
 
 if __name__ == '__main__':
     main()
