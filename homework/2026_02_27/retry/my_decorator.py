614c93efbdc0799e90ffbac8a85b0903f6d90caa
skilful-kit <skilfulkitsigma@gmail.com>
2026-04-29 20:34:25 +0200

    Update my_decorator.py

--- homework/2026_02_27/retry/my_decorator.py
+++ homework/2026_02_27/retry/my_decorator.py
@@ -1,2 +1,14 @@
 def retry(count):
-    pass
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(count):
+                try:
+                    return func(*args, **kwargs)
+                except ValueError:
+                    continue
+                except OSError:
+                    print(f"{func.__name__} raise OSError exception.")
+                    continue
+            return None
+        return wrapper
+    return decorator
